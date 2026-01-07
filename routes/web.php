10967a34995c1c698a204704884c9b0d4a8535cb
Primary nutthawat <primary.nutthawat.w@aitinnovation.co.th>
2026-01-07 17:54:23 +0700

    Enhance Login component with SVG graphics for improved visual appeal. Update routing in web.php to group authenticated routes, ensuring better organization and access control for profile and dashboard pages.

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,20 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', [PageController::class, 'home']);
-Route::get('/profile', [PageController::class, 'profile']);
-Route::get('/login', [PageController::class, 'login']);
 Route::get('/weather', [PageController::class, 'weather']);
+Route::get('/login', [PageController::class, 'login']);
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+
+Route::get('/profile', [PageController::class, 'profile']);
 Route::get('/card', [PageController::class, 'card']);
-
-
+Route::get('dashboard', function () {
+    return Inertia::render('dashboard');
+})->name('dashboard');
 Route::resource('books', BookController::class)->except(['show']);
+// });
+
 require __DIR__ . '/api.php';
